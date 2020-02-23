caf56921c4dbef69389a2c020a7790769fff2248
Daniel Stahl <danstahl1138@gmail.com>
2020-02-23 09:47:41 -0600

    started down firebase path

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+const config = require('./config.json')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Initialize Firebase
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  //signInSuccessUrl: '/',
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false
+  },
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
+  ]
+};
+
+const DevHome = ({ token }) => <p>Hello, <a onClick={() => firebase.auth().signOut()}>Sign-out</a></p>
+const SignInScreen = () => {
+  //const [isSignedIn, setIsSignedIn] = useState(false)
+  const [user, setUser] = useState({})
+  const [token, setToken] = useState("")
+  console.log(token)
+  firebase.auth().onAuthStateChanged(user => {
+    user.getIdToken(true).then(setToken).catch(err => console.log(err))
+    setUser(user)
+  })
+
+  console.log(user)
+  return !!user ? <DevHome /> : (
+    <div>
+      <h1>My App</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default SignInScreen;

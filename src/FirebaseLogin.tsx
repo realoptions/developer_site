5a4f9d51c9bd4f0a2edb91f997fcef1a14d63ff4
Daniel Stahl <danstahl1138@gmail.com>
2026-03-05 16:07:51 -0600

    moved to vite

--- src/FirebaseLogin.tsx
+++ src/FirebaseLogin.tsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import { GoogleLoginButton, FacebookLoginButton, GithubLoginButton } from 'react-social-login-buttons'
-import { GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider, signInWithPopup, Auth } from 'firebase/auth'
-import { Col, Row } from 'antd';
-const googleProvider = new GoogleAuthProvider()
-const facebookProvider = new FacebookAuthProvider()
-const githubProvider = new GithubAuthProvider()
+import {
+  GoogleLoginButton,
+  FacebookLoginButton,
+  GithubLoginButton,
+} from "react-social-login-buttons";
+import {
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+  type Auth,
+} from "firebase/auth";
+import { Col, Row } from "antd";
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 export const LoginButton = ({ auth }: { auth: Auth }) => {
-    return <>
-        <Row>
-            <Col xs={0} md={6}></Col>
-            <Col xs={24} md={12}>
-                <GoogleLoginButton
-                    onClick={() => {
-                        signInWithPopup(auth, googleProvider)
-                    }}
-                />
-            </Col>
-        </Row >
-        <Row>
-            <Col xs={0} md={6}></Col>
-            <Col xs={24} md={12}>
-                <FacebookLoginButton
-                    onClick={() => {
-                        signInWithPopup(auth, facebookProvider)
-                    }}
-                />
-            </Col>
-        </Row >
-        <Row>
-            <Col xs={0} md={6}></Col>
-            <Col xs={24} md={12}>
-                <GithubLoginButton
-                    onClick={() => {
-                        signInWithPopup(auth, githubProvider)
-                    }}
-                />
-            </Col>
-        </Row >
+  return (
+    <>
+      <Row>
+        <Col xs={0} md={6}></Col>
+        <Col xs={24} md={12}>
+          <GoogleLoginButton
+            onClick={() => {
+              signInWithPopup(auth, googleProvider);
+            }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={0} md={6}></Col>
+        <Col xs={24} md={12}>
+          <FacebookLoginButton
+            onClick={() => {
+              signInWithPopup(auth, facebookProvider);
+            }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={0} md={6}></Col>
+        <Col xs={24} md={12}>
+          <GithubLoginButton
+            onClick={() => {
+              signInWithPopup(auth, githubProvider);
+            }}
+          />
+        </Col>
+      </Row>
     </>
-}
-export default LoginButton
+  );
+};
+export default LoginButton;
